03f8f54d4081fa653fb38bf44fce4a176c78ca06
costa <costanzosara96@gmail.com>
2023-11-30 13:39:38 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// In questo esercizio di codifica dovrai completare la funzione qui sotto.
+
+
+// Input 
+
+// L'input di questo metodo è un array contenente 10 numeri interi (numbersArray).
+
+// Output
+
+// Come output restituisce un numero intero (indice).
+
+
+// Specifiche:
+
+// La funzione deve essere implementata come segue:
+
+// "Dato come input un array di interi positivi e negativi, individuare la posizione della prima occorrenza del numero 1 (l'indice del primo elemento dell'array contenente 1). 
+// Se non presente, restituire -1".
+
+
+
+// Esempi:
+
+// Ingresso: [9,6,3,13,1,2,3,4,5,22] Uscita: 4
+
+// Ingresso: [1,1,0,0,1,1,0,0,1,1] Uscita: 0
+
+// Ingresso: [-1,52,-2,2,-3,3,4,76,80,-67] Uscita: -1
